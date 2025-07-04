53432aa2c74e195302a01d36839b65ea4a5a64d7
abdullansari <abdullansari1849@gmail.com>
2025-07-04 12:15:36 +0400

    Reconfigured App.tsx and wrapped in a BrowserRouter container.

--- src/containers/App/App.test.tsx
+++ src/containers/App/App.test.tsx
@@ -1,5 +1,10 @@
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => jest.fn(),
+}));
+
 import React from "react";
-import SpeechRecordScreen from "../SubmitSteps/RecordingsSteps/SpeechRecordScreen";
+import SpeechRecordScreen from "../SubmitSteps/RecordingsSteps/SpeechRecording/SpeechRecordScreen";
 
 const App: React.FC = () => {
   return (
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import SpeechRecordScreen from "../SubmitSteps/RecordingsSteps/SpeechRecording/SpeechRecordScreen";
 
 const App: React.FC = () => {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
-      <h1>Hello Virufy Team 👋</h1>
-      <p style={{ fontSize: '1.2rem', marginTop: '1rem' }}>
-        This is the structure of the team — let’s fit in, collaborate, and build something great on top of it! 🚀
-      </p>
-    </div>
+    <BrowserRouter>
+      <SpeechRecordScreen />
+    </BrowserRouter>
   );
 };
 
