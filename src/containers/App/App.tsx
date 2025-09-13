5277bf241b01a3d69da36707bbca24f2d9887520
NoellaAbraham <abrahamnoella05@gmail.com>
2025-09-13 22:17:09 +0400

    encrypted header in login page instead of upload page

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -9,9 +9,7 @@ import CoughRecordScreen from "../SubmitSteps/RecordingsSteps/CoughRecordScreen"
 import UploadCompleteCough from "../SubmitSteps/RecordingsSteps/UploadCompleteCough";
 import ConsentScreen from "./ConsentPage";
 import Clinical_Login from "./Clinical_Login";
-import ProtectedRoute from "../../components/ProtectedRoute";
 
-// --- Import your new ProtectedRoute component ---
 //import ProtectedRoute from "../../components/ProtectedRoute";
 
 // Main App with routes and language support
